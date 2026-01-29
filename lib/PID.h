8ada999f72b7b77895586d1fc2562236c2815a76
Nomu1Nomu1 <isaiah.tino@gmail.com>
2026-01-29 14:30:43 +0700

    update

--- lib/PID.h
+++ lib/PID.h
@@ -1,15 +1,21 @@
 #ifndef PID_H
 #define PID_H
 
+class PID {
+public:
+    PID(float p, float i, float d, float minVal, float maxVal);
+    float calculate(float target, float current);
+    void reset();
 
-
-class PID{
-    public:
-        float kp, ki, kd;
-        float prevError, integral;
-        PID(float p,float i,float d);
-        float calculate(float target, float current);
-
+private:
+    float kp, ki, kd;
+    float outMin, outMax;
+    float integral, prevError;
+    float integralLimit;
+    unsigned long lastTime;
+    float prevComponent;           // Menyimpan nilai 'current' sebelumnya
+    float lastFilteredDerivative;  // Menyimpan hasil filter derivatif terakhir
+    float alpha;                   // Koefisien filter (0.0 - 1.0)
 };
 
 #endif
